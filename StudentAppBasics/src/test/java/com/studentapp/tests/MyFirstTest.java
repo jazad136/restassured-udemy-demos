094432ad1c31a1d58dd412a1696f9f436e70e55d
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-07-29 09:30:07 -0400

    use queryParams in JUnit GET method

--- StudentAppBasics/src/test/java/com/studentapp/tests/MyFirstTest.java
+++ StudentAppBasics/src/test/java/com/studentapp/tests/MyFirstTest.java
@@ -1,5 +1,7 @@
 package com.studentapp.tests;
 import static io.restassured.RestAssured.*;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -44,5 +46,21 @@ public class MyFirstTest {
                 .statusCode(200)
                 .when()
                 .get("http://localhost:8080/student/list");
+    }
+    
+    @DisplayName("Get a CS Student from the list")
+    @Test
+    void getSingleCSStudent() { 
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("programme", "Computer Science");
+        params.put("limit", 1);
+        
+        var response = given()
+//                .queryParam("programme", "Computer Science")
+//                .queryParam("limit", 1)
+                .queryParams(params)
+                .when()
+                .get("http://localhost:8080/student/list");
+        response.prettyPrint();
     }
 }
