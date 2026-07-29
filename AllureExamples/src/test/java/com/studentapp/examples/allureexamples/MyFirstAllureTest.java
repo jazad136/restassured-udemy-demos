f51bce729848d407ec36109b012b9736ace0e30e
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-07-29 15:35:47 -0400

    Add new AllureExamples directory with first attempt at Adding Allure Reports

---
+++ AllureExamples/src/test/java/com/studentapp/examples/allureexamples/MyFirstAllureTest.java
@@ -0,0 +1,15 @@
+package com.studentapp.examples.allureexamples;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author JonathanSaddler
+ */
+public class MyFirstAllureTest {
+    @Test
+    public void tc1() { 
+        assertTrue(true);
+    }
+}
