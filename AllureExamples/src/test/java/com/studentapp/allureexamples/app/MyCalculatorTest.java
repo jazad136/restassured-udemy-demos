d9d15b784d8343f1ea90bec92bb48f59b7423434
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-07-31 15:08:36 -0400

    Add links, start on epics/features

--- AllureExamples/src/test/java/com/studentapp/allureexamples/app/MyCalculatorTest.java
+++ AllureExamples/src/test/java/com/studentapp/allureexamples/app/MyCalculatorTest.java
@@ -1,10 +1,18 @@
 package com.studentapp.allureexamples.app;
 import com.studentapp.allureexamples.app.MySimpleCalculator;
 import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
+import io.qameta.allure.Story;
+import io.qameta.allure.TmsLink;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+@Epic("All tools for calculation")
+@Feature("Calculator to perform mathematical operations")
 @DisplayName("Calculator to perform mathematical operations")
 public class MyCalculatorTest {
     @Test
@@ -20,18 +28,27 @@ public class MyCalculatorTest {
         assertThat(calculate.add2Num(3.0, 5.0), equalTo(8.0));
     }
     
+    
+    @TmsLink("TestLink")
+    @DisplayName("Calculate difference of 2 numbers")
+    @Story("Calculate difference of 2 numbers")
+    @Test
     public void tc2(){
 
         MySimpleCalculator calculator = new MySimpleCalculator();
         assertThat(calculator.sub2Num(5.0,4.0) ,equalTo(1.0));
     }
     
+    @Link("Software_testing") // @Link did not work
+    @Test
     public void tc3(){
 
         MySimpleCalculator calculator = new MySimpleCalculator();
         assertThat(calculator.mul2Num(5.0,4.0) ,equalTo(20.0));
     }
     
+    @Issue("Issue")
+    @Test
     public void tc4(){
 
         MySimpleCalculator calculator = new MySimpleCalculator();
--- AllureExamples/src/test/java/com/studentapp/allureexamples/app/MyFirstAllureTest.java
+++ AllureExamples/src/test/java/com/studentapp/allureexamples/app/MyFirstAllureTest.java
@@ -1,10 +1,13 @@
 package com.studentapp.allureexamples.app;
 
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Assert the value true is true")
+@Epic("All tools for calculation")
+@Feature("I just pass")
 public class MyFirstAllureTest {
     @DisplayName("My first allure test")
     @Test
